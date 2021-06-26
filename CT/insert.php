5ecf61ff9744800e302baf67aed26c40ffd22501
John Leonard Paual <john@quantumcrowd.io>
2021-06-26 14:25:13 +0800

    Added visitor API call

--- CT/insert.php
+++ CT/insert.php
@@ -33,12 +33,37 @@
      
     // attempt insert query execution
 
-    $sql = "INSERT INTO tracing (name,age,contact,time,date,address) VALUES ('$name','$age','$contact','$time','$date','address')";
+    $sql = "INSERT INTO tracing (name,age,contact,time,date,address) VALUES ('$name','$age','$contact','$time','$date','$address')";
 
     if(mysqli_query($link, $sql)){
+        
+        $data_array = [
+            'name' => $name,
+            'age' => $age,
+            'address' => $address,
+            'contactNumber' => $contact
+          ];
 
+        $data = json_encode($data_array);
 
- } else{
+        $url = 'https://attendance-system-app.herokuapp.com/api/visitors';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        // OPTIONS:
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Authorization: da37dbde-49f8-49bc-9340-2e04ecb9fc6a',
+            'Content-Type: application/json',
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        // EXECUTE:
+        $result = curl_exec($curl);
+        if(!$result){die("Connection Failure");}
+        curl_close($curl);
+
+    } else{
 
         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 
